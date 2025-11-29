76beec72cf689ff17d50160d53435073bf16dd6f
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2025-11-30 01:31:24 +0900

    Add checkboxes and a delete button to the ToDo app and improve the styling

--- 02-js-basics/app.js
+++ 02-js-basics/app.js
@@ -1,19 +1,49 @@
-function addTodo() {
-  const todoInput = document.getElementById('new-todo-input');
-  const todoList = document.getElementById('todo-list');
+const todoInput = document.getElementById('new-todo-input');
+todoInput.addEventListener('keydown', function (e) {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+});
 
+const addButton = document.getElementById('add-todo-button');
+addButton.addEventListener('click', addTodo);
+
+function addTodo() {
   if (todoInput.value === '') {
     alert('ToDoの内容を入力してください！！');
     return;
   }
 
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox'
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      textNode.className = 'completed';
+      newTodoItem.removeChild(deleteButton);
+    } else {
+      textNode.className = '';
+      newTodoItem.appendChild(deleteButton);
+    }
+  });
+
+  const textNode = document.createElement('span');
+  textNode.textContent = todoInput.value;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'delete-button';
+  deleteButton.addEventListener('click', function () {
+    const todoList = document.getElementById('todo-list');
+    todoList.removeChild(newTodoItem);
+  });
+
   const newTodoItem = document.createElement('li');
-  // newTodoItem.textContent = todoInput.value;
-  newTodoItem.innerHTML = `<input type="checkbox"> ${todoInput.value} <button class="delete-button">削除</button>`;
+  newTodoItem.appendChild(checkbox);
+  newTodoItem.appendChild(textNode);
+  newTodoItem.appendChild(deleteButton);
+
+  const todoList = document.getElementById('todo-list');
   todoList.appendChild(newTodoItem);
 
   todoInput.value = '';
 }
-
-const button = document.getElementById('add-todo-button');
-button.addEventListener('click', addTodo);
